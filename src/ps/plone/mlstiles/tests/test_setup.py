db45fff5efd7111ad97ab5e7b0a13a3a13a865d8
Thomas Massmann <thomas@propertyshelf.com>
2014-05-26 16:34:26 +0200

    Register tiles and make them available for collective.cover.

--- src/ps/plone/mlstiles/tests/test_setup.py
+++ src/ps/plone/mlstiles/tests/test_setup.py
@@ -2,14 +2,21 @@
 """Test Setup of ps.plone.mlstiles."""
 
 # python imports
-import unittest2 as unittest
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+# zope imports
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
 
 # local imports
 from ps.plone.mlstiles.testing import PS_PLONE_MLSTILES_INTEGRATION_TESTING
 
 
 class TestSetup(unittest.TestCase):
-    """Validata setup process for ps.plone.mlstiles."""
+    """Validate setup process for ps.plone.mlstiles."""
 
     layer = PS_PLONE_MLSTILES_INTEGRATION_TESTING
 
@@ -32,3 +39,29 @@ class TestSetup(unittest.TestCase):
         """Validate that plone.mls.listing is installed."""
         qi = self.portal.portal_quickinstaller
         self.assertTrue(qi.isProductInstalled('plone.mls.listing'))
+
+    def test_tiles_registered(self):
+        """Validate that the tiles are registered."""
+        registry = getUtility(IRegistry)
+        key = 'plone.app.tiles'
+        self.assertIn(
+            'ps.plone.mlstiles.listings.collection',
+            registry.records.get(key).value,
+        )
+        self.assertIn(
+            'ps.plone.mlstiles.listings.recent',
+            registry.records.get(key).value,
+        )
+
+    def test_tiles_available(self):
+        """Validate that the tiles are available within a cover."""
+        registry = getUtility(IRegistry)
+        key = 'collective.cover.controlpanel.ICoverSettings.available_tiles'
+        self.assertIn(
+            'ps.plone.mlstiles.listings.collection',
+            registry.records.get(key).value,
+        )
+        self.assertIn(
+            'ps.plone.mlstiles.listings.recent',
+            registry.records.get(key).value,
+        )
