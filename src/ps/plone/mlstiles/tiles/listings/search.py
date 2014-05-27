456eb9a10e4f0e8c01b0cd72fd51d732f0ee0a04
Thomas Massmann <thomas@propertyshelf.com>
2014-05-27 12:02:04 +0200

    Added form fields for tile configuration.

--- src/ps/plone/mlstiles/tiles/listings/search.py
+++ src/ps/plone/mlstiles/tiles/listings/search.py
@@ -12,6 +12,7 @@ from plone.app.uuid.utils import uuidToObject
 from plone.directives import form
 from plone.mls.listing.browser import listing_search
 from plone.tiles.interfaces import ITileDataManager
+from plone.mls.listing.i18n import _ as _mls
 from plone.uuid.interfaces import IUUID
 from z3c.form import field, button
 from zope import schema
@@ -63,6 +64,96 @@ class IListingSearchTile(base.IPersistentCoverTile):
         title=_cc(u'Header'),
     )
 
+    form_listing_type = schema.Text(
+        required=False,
+        title=_mls(u'Listing Type'),
+    )
+
+    form_location_state = schema.Text(
+        required=False,
+        title=_mls(u'State'),
+    )
+
+    form_location_county = schema.Text(
+        required=False,
+        title=_mls(u'County'),
+    )
+
+    form_location_district = schema.Text(
+        required=False,
+        title=_mls(u'District'),
+    )
+
+    form_location_city = schema.Text(
+        required=False,
+        title=_mls(u'City/Town'),
+    )
+
+    form_price_min = schema.Text(
+        required=False,
+        title=_mls(u'Price (Min)'),
+    )
+
+    form_price_max = schema.Text(
+        required=False,
+        title=_mls(u'Price (Max)'),
+    )
+
+    form_location_type = schema.Text(
+        required=False,
+        title=_mls(u'Location Type'),
+    )
+
+    form_geographic_type = schema.Text(
+        required=False,
+        title=_mls(u'Geographic Type'),
+    )
+
+    form_view_type = schema.Text(
+        required=False,
+        title=_mls(u'View Type'),
+    )
+
+    form_object_type = schema.Text(
+        required=False,
+        title=_mls(u'Object Type'),
+    )
+
+    form_ownership_type = schema.Text(
+        required=False,
+        title=_mls(u'Ownership Type'),
+    )
+
+    form_beds = schema.Text(
+        required=False,
+        title=_mls(u'Bedrooms'),
+    )
+
+    form_baths = schema.Text(
+        required=False,
+        title=_mls(u'Bathrooms'),
+    )
+
+    form_air_condition = schema.Text(
+        required=False,
+        title=_mls(u'Air Condition'),
+    )
+
+    form_pool = schema.Text(
+        required=False,
+        title=_mls(u'Pool'),
+    )
+
+    form_jacuzzi = schema.Text(
+        required=False,
+        title=_mls(u'Jacuzzi'),
+    )
+
+    form_lot_size = schema.Text(
+        required=False,
+        title=_mls(u'Lot Size'),
+    )
+
     footer = schema.TextLine(
         required=False,
         title=_cc(u'Footer'),
@@ -84,6 +175,42 @@ class ListingSearchTile(base.PersistentCoverTile):
     index = ViewPageTemplateFile('search.pt')
 
     header = FieldProperty(IListingSearchTile['header'])
+
+    form_listing_type = FieldProperty(IListingSearchTile['form_listing_type'])
+    form_location_state = FieldProperty(
+        IListingSearchTile['form_location_state']
+    )
+    form_location_county = FieldProperty(
+        IListingSearchTile['form_location_county']
+    )
+    form_location_district = FieldProperty(
+        IListingSearchTile['form_location_district']
+    )
+    form_location_city = FieldProperty(
+        IListingSearchTile['form_location_city']
+    )
+    form_price_min = FieldProperty(IListingSearchTile['form_price_min'])
+    form_price_max = FieldProperty(IListingSearchTile['form_price_max'])
+    form_location_type = FieldProperty(
+        IListingSearchTile['form_location_type']
+    )
+    form_geographic_type = FieldProperty(
+        IListingSearchTile['form_geographic_type']
+    )
+    form_view_type = FieldProperty(IListingSearchTile['form_view_type'])
+    form_object_type = FieldProperty(IListingSearchTile['form_object_type'])
+    form_ownership_type = FieldProperty(
+        IListingSearchTile['form_ownership_type']
+    )
+    form_beds = FieldProperty(IListingSearchTile['form_beds'])
+    form_baths = FieldProperty(IListingSearchTile['form_baths'])
+    form_air_condition = FieldProperty(
+        IListingSearchTile['form_air_condition']
+    )
+    form_pool = FieldProperty(IListingSearchTile['form_pool'])
+    form_jacuzzi = FieldProperty(IListingSearchTile['form_jacuzzi'])
+    form_lot_size = FieldProperty(IListingSearchTile['form_lot_size'])
+
     footer = FieldProperty(IListingSearchTile['footer'])
     uuid = FieldProperty(IListingSearchTile['uuid'])
 
